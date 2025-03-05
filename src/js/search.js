13494ba4323c1c4efe4468a9ae9e51e8c5cec3ff
Diego Vilhalva <diegorocha450@yahoo.com>
2025-03-05 09:16:59 -0300

    header and ripple effect added

--- src/js/search.js
+++ src/js/search.js
@@ -1,6 +1,19 @@
-/**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
- */
+
 
 "use strict";
+
+import { ripple } from "./utils/ripple";
+
+
+
+const $header = document.querySelector("[data-header]")
+
+window.addEventListener("scroll",() => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active")
+})
+
+
+
+const $rippleElems = document.querySelectorAll("[data-ripple]")
+
+$rippleElems.forEach($rippleElem => ripple($rippleElem))
--- src/js/utils/ripple.js
+++ src/js/utils/ripple.js
@@ -1,6 +1,31 @@
-/**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
- */
+
 
 "use strict";
+
+export const ripple = function($rippleElem){
+    $rippleElem.addEventListener("pointerdown",function (e) {
+        e.stopImmediatePropagation()
+
+
+        const $ripple = document.createElement("div")
+        $ripple.classList.add("ripple")
+
+        this.appendChild($ripple)
+
+
+        const removeRipple = () => {
+            $ripple.animate({
+                opacity:0
+            },{
+                fill:"forwards",
+                duration:200
+            })
+            setTimeout(() => {
+               $ripple.remove() 
+            }, 1000);
+        } 
+
+        this.addEventListener("pointerup",removeRipple)
+        this.addEventListener("pointerleave",removeRipple)
+    })
+}
