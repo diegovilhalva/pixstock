12c5aa83e3c7cd5f1acc4f542b042fa83ade66d3
Diego Vilhalva <diegorocha450@yahoo.com>
2025-03-07 10:58:04 -0300

    fetch and render collections functionality added

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,10 @@
 "use strict";
 
 import { client } from "./api_configure"
+import { collectionCard } from "./collection_card";
 import { photoCard } from "./photo_card";
 import { gridInit, updateGrid } from "./utils/masonry_grid";
+import { videoCard } from "./video_card";
 
 const $photoGrid = document.querySelector("[data-photo-grid]")
 
@@ -25,3 +27,41 @@ client.photos.curated({page:1,per_page:20},data => {
         updateGrid($photoCard,photoGrid.columnsHeight,photoGrid.$columns)
     })  
 })
+
+
+
+const $videoGrid = document.querySelector("[data-video-grid]");
+
+$videoGrid.innerHTML = `<div class="skeleton"></div>`.repeat(18);
+
+client.videos.popular({ per_page: 20 }, data => {
+
+  $videoGrid.innerHTML = "";
+  const videoGrid = gridInit($videoGrid);
+
+  data.videos.forEach(video => {
+
+    const $videoCard = videoCard(video);
+
+    updateGrid($videoCard, videoGrid.columnsHeight, videoGrid.$columns);
+
+  });
+
+})
+
+
+
+const  $collectionGrid = document.querySelector("[data-collection-grid]")
+
+
+client.collections.featured({ per_page: 18 }, data => {
+
+    data.collections.forEach(collection => {
+  
+      const  $collectionCard = collectionCard(collection);
+  
+      $collectionGrid.appendChild($collectionCard);
+  
+    });
+  
+  });
--- src/js/collection_card.js
+++ src/js/collection_card.js
@@ -1,6 +1,34 @@
-/**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
- */
+
 
 "use strict";
+
+import { ripple } from "./utils/ripple";
+
+export const collectionCard = collection => {
+
+    const  root = window.location.origin;
+  
+    const {
+      id,
+      title,
+      media_count
+    } = collection;
+  
+    const $card = document.createElement("div");
+    $card.classList.add("grid-card", "two-line", "list-item");
+    $card.setAttribute("title", title);
+  
+    $card.innerHTML = `
+      <div>
+        <h3 class="body-large">${title}</h3>
+  
+        <p class="body-medium label">${media_count} media</p>
+      </div>
+  
+      <a href="${root}/pages/collections/collection_detail.html?collectionId=${id}&title=${title}" class="state-layer"></a>
+    `;
+  
+    ripple($card);
+  
+    return $card;
+}  
