f5573732d681f37ddc55f39e421ee50cc34e4b2f
Diego Vilhalva <diegorocha450@yahoo.com>
2025-03-10 10:07:18 -0300

    render photos in photo page

--- public/pages/photos/photos.js
+++ public/pages/photos/photos.js
@@ -2,8 +2,11 @@
 "use strict";
 
 import { updateUrl } from "../../../src/js/utils/updateUrl";
-
-
+import { client } from "../../../src/js/api_configure"
+import { photoCard }  from "../../../src/js/photo_card"
+import { gridInit, updateGrid }  from "../../../src/js/utils/masonry_grid"
+import { urlDecode } from "../../../src/js/utils/urlDecode";
+import { filter } from "../../../src/js/filter";
 const $filterBar = document.querySelector("[data-filter-bar]")
 
 $filterBar.style.display = window.location.search ? "flex" : "none"
@@ -29,3 +32,39 @@ const  title = searchObj ? `${searchObj.query} photos` : "Curated photos"
 
 $title.textContent = title
 document.title  = title
+
+
+
+const renderPhotos = function (currentPage) {
+    client.photos[searchObj ? "search" : "curated"]({...searchObj,per_page:perPage,page:currentPage},data => {
+        totalPage = Math.ceil(data.total_results / perPage)
+
+        data.photos.forEach(photo => {
+            const $photoCard = photoCard(photo)
+
+            updateGrid($photoCard,photoGrid.columnsHeight,photoGrid.$columns)
+        })
+        isLoaded = true
+        if(currentPage >= totalPage) $loader.style.display = 'none' 
+    })
+
+
+}
+
+renderPhotos(currentPage)
+
+
+const $loader = document.querySelector("[data-loader]");
+let isLoaded = true;
+
+window.addEventListener("scroll", function () {
+
+    if ($loader.getBoundingClientRect().top < (window.innerHeight * 2) && currentPage <= totalPage && isLoaded) {
+  
+      currentPage++;
+      renderPhotos(currentPage);
+      isLoaded = false;
+  
+    }
+  
+});
