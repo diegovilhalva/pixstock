93023f124bcb1ab7206686eba9bc6da70aa9da61
Diego Vilhalva <diegorocha450@yahoo.com>
2025-03-12 08:37:03 -0300

    render collections in collections page

--- public/pages/collections/collection.js
+++ public/pages/collections/collection.js
@@ -1,6 +1,41 @@
-/**
- * @copyright codewithsadee 2023
- * @author sadee <codewithsadee@gmail.com>
- */
+
 
 "use strict";
+
+import { client } from "../../../src/js/api_configure";
+import { collectionCard } from "../../../src/js/collection_card"
+
+const $collectionGrid = document.querySelector("[data-collection-grid]")
+const perPage = 36
+let currentPage = 1
+let totalPage = 0
+
+const loadCollections = function (page) {
+    client.collections.featured({ per_page: perPage, page: page }, data => {
+        totalPage = Math.ceil(data.total_results / perPage)
+
+        data.collections.forEach(collection => {
+            const $collectionCard = collectionCard(collection)
+            $collectionGrid.appendChild($collectionCard)
+        })
+
+        isLoaded = true;
+        (currentPage >= totalPage) && ($loader.style.display = "none");
+    })
+}
+
+
+loadCollections(currentPage)
+
+const $loader = document.querySelector("[data-loader]")
+let isLoaded = false
+
+const loadMore = function () {
+    if ($loader.getBoundingClientRect().top < (window.innerHeight * 2) && currentPage <= totalPage && isLoaded) {
+        currentPage++
+        loadCollections(currentPage)
+        isLoaded = false
+    }
+}
+
+window.addEventListener("scroll", loadMore)
